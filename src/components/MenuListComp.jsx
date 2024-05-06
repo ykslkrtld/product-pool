6f6b0f3ddf35f2ee179abb8cb8ea502d21ea5ac0
yksl <yukselkurtuldu9@gmail.com>
2024-05-06 22:45:54 +0300

    edit

--- src/components/MenuListComp.jsx
+++ src/components/MenuListComp.jsx
@@ -9,8 +9,11 @@ import InventoryIcon from "@mui/icons-material/Inventory"
 import StoreIcon from "@mui/icons-material/Store"
 import StarsIcon from "@mui/icons-material/Stars"
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
+import { useNavigate } from "react-router-dom"
 
 const MenuListComp = () => {
+  const navigate = useNavigate()
+
   const icons = [
     {
       title: "Dashboard",
@@ -48,7 +51,12 @@ const MenuListComp = () => {
     <div>
       <List>
         {icons.map((item, index) => (
-          <ListItem key={item.title} disablePadding>
+          <ListItem
+            key={index}
+            disablePadding
+            onClick={() => navigate(item.path)}
+            sx={{color:"white", "& .MuiSvgIcon-root": {color:"white"}}}
+          >
             <ListItemButton>
               <ListItemIcon>{item.iconName}</ListItemIcon>
               <ListItemText primary={item.title} />
