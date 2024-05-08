bcf704bd837ca27d21ec2f2983ea05020479719d
yksl <yukselkurtuldu9@gmail.com>
2024-05-08 13:51:45 +0300

    edit

--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -1,31 +1,30 @@
-import * as React from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import { Button, TextField } from "@mui/material";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
-export default function ModalComp({open, setOpen}) {
-  
+export default function ModalComp({ open, setOpen }) {
   return (
     <div>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={open}
-        onClose={()=> setOpen(false)}
+        onClose={() => setOpen(false)}
         closeAfterTransition
         slots={{ backdrop: Backdrop }}
         slotProps={{
@@ -35,13 +34,35 @@ export default function ModalComp({open, setOpen}) {
         }}
       >
         <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+          <Box sx={style} display="flex" flexDirection="column" gap="1rem" component="form">
+            <TextField
+              id="name"
+              name="name"
+              label="Firm Name"
+              variant="outlined"
+              required
+            />
+            <TextField
+              id="phone"
+              name="phone"
+              label="Phone"
+              variant="outlined"
+            />
+            <TextField
+              id="address"
+              name="address"
+              label="Address"
+              variant="outlined"
+              required 
+            />
+            <TextField
+              id="image"
+              name="image"
+              label="Image"
+              variant="outlined"
+              required type="url"
+            />
+      <Button variant="contained">ADD FIRM</Button>
           </Box>
         </Fade>
       </Modal>
