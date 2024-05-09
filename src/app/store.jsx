9068e3af0a552dabe1b6bf69b659b16e4521df59
yksl <yukselkurtuldu9@gmail.com>
2024-05-09 23:21:59 +0300

    redux persist

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,12 +2,36 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import getDataReducer from "../features/getDataSlice"
 
+import { persistStore, persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+// import storage from 'redux-persist/lib/storage/session'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
     getDatas: getDataReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store)
 export default store;
 
